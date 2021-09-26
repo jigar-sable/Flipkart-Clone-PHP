e62629c34a0fe3ed19c2cdaad013e2fb72bbe657
Jigar Sable <jigarsable21@gmail.com>
2021-09-26 13:05:17 +0530

    php template init

---
+++ Frontend/node_modules/win-node-env/index.js
@@ -0,0 +1,42 @@
+const { spawnSync } = require('child_process');
+const parse = require('./command-parser');
+
+const warn = () => console.warn('Note: This command was run via npm module \'win-node-env\'');
+const error = (error, exitCode) => {
+  warn();
+  if (error) console.error(error);
+  process.exitCode = exitCode || 1;
+}
+
+module.exports = firstEnvVar => {
+
+  if (!process.argv[2]) {
+    error('Error: No command was given');
+    return;
+  }
+
+  process.argv[2] = firstEnvVar + process.argv[2];
+
+  const args = process.argv.slice(2);
+  const combinedString = args.join(' ');
+  const separateCommands = combinedString.split(/;/g).map(s => s.trim());
+  const separateParsedCommands = separateCommands.map(s => parse(s.split(/ /g)));
+
+  separateParsedCommands.forEach(({ env, cmd }, i) => {
+    const { status: exitCode } = run(cmd, env);
+    if (exitCode && i === separateParsedCommands.length - 1) {
+      error(null, exitCode);
+    }
+  });
+};
+
+function run(cmd, env) {
+  if (!cmd.length) {
+    error('Error: No command was given');
+    return;
+  }
+
+  const newEnv = Object.assign({}, process.env, env);
+
+  return spawnSync(cmd[0], cmd.slice(1), { stdio: 'inherit', shell: true, env: newEnv });
+}
---
+++ partials/_recommended_items.php
@@ -0,0 +1,308 @@
+    <!-- recommended items section -->
+    <section id="recommended" class="bg-white w-full shadow overflow-hidden">
+        <!-- header -->
+        <div class="flex px-6 py-4 justify-between items-center">
+            <div class="title flex flex-col gap-0.5">
+                <h1 class="text-xl font-medium">Recommended Items</h1>
+                <p class="text-sm text-gray-400">Inspired by Your Interest</p>
+            </div>
+            <a href="#" class="bg-primary-blue text-xs font-medium text-white px-5 py-2.5 rounded-sm shadow-lg">VIEW ALL</a>
+        </div>
+        <hr>
+        <!-- header -->
+        
+        <!-- products container -->
+        <div class="flex items-center justify-between owl-carousel owl-theme p-1 relative">
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+            <!-- one product -->
+            <div class="flex flex-col items-center gap-2 px-2 py-6 relative">
+                <!-- image & product title -->
+                <a href="#" class="flex flex-col items-center text-center group">
+                <div class="w-36 h-36">
+                    <img class="w-full h-full object-contain" src="https://rukminim1.flixcart.com/image/312/312/kk1h5e80/refrigerator-new/g/y/y/rt28a3021s8-hl-1-2021-samsung-original-imafzhedkazavggn.jpeg" alt="">
+                </div>
+                <h2 class="text-sm mt-4 group-hover:text-primary-blue">SAMSUNG 230 L Direct Cool Single Door 3 Star Refrigerator</h2>
+                </a>
+                <!-- image & product title -->
+
+                <!-- product description -->
+                <div class="flex flex-col gap-2 items-center">
+                        <!-- rating badge -->
+                        <span class="text-sm text-gray-500 font-medium flex gap-2 items-center">
+                            <span class="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">4.3 <i class="material-icons md-12">star</i></span>
+                            <span>(7,345)</span>
+                        </span>
+                        <!-- rating badge -->
+
+                        <!-- price container -->
+                        <div class="flex items-center gap-1.5 text-md font-medium">
+                            <span>₹16,790</span>
+                            <span class="text-gray-500 line-through text-xs">₹18,890</span>
+                            <span class="text-xs text-primary-green">15%&nbsp;off</span>
+                        </div>
+                        <!-- price container -->
+                </div>
+                <!-- product description -->
+
+                <!-- wishlist badge -->
+                <i class="material-icons absolute top-5 right-2 text-gray-300 cursor-pointer hover:text-red-500 md-16">favorite</i>
+                <!-- wishlist badge -->
+
+            </div>
+
+        </div>
+        <!-- products container -->
+    </section>
+    <!-- recommended items section -->
