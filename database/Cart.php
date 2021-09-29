9bd4a3c788aa8c34cb2583ee604b5aa26e346bde
Jigar Sable <jigarsable21@gmail.com>
2021-09-29 18:40:27 +0530

    cart class init

---
+++ database/Cart.php
@@ -0,0 +1,17 @@
+<?php
+
+class Cart {
+    public $db = null;
+
+    public function __construct(DBController $db) {
+        if(!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    
+}
+
+
+
+
+?>
