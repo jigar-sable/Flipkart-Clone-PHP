047769ddd5ca328c850003f1373a7a70c1c200e7
Jigar Sable <jigarsable21@gmail.com>
2021-10-03 13:25:55 +0530

    category nav db

--- partials/_categories_nav.php
+++ partials/_categories_nav.php
@@ -3,19 +3,14 @@
 
         <!-- categories container -->
         <div class="flex items-center justify-between p-0.5">
-            
+        <?php
+        foreach($product->getData('category_nav') as $category):
+        ?>
             <!-- categories -->
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">Electronics <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">TVs & Appliances <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">Men <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">Women <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">Baby & Kids <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">Home & Furniture <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="#" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group">Sports, Books & More <span class="material-icons md-16 text-gray-400 group-hover:text-primary-blue">expand_more</span></a>
-            <a  href="https://www.flipkart.com/travel/flights?otracker=nmenu_Flights" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue">Flights</a>
-            <a  href="https://www.flipkart.com/offers-store" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue">Offer Zone</a>
-            <a  href="https://www.flipkart.com/grocery-supermart-store?marketplace=GROCERY&otracker=nmenu_Grocery" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue">Grocery</a>
-            <!-- categories -->
+            <a  href="<?php echo $category['cat_url']; ?>" class="text-sm p-2 text-gray-800 font-medium hover:text-primary-blue flex items-center gap-0.5 group"><?php echo $category['cat_title']; ?></a>
+        <?php
+        endforeach;
+        ?>
 
         </div>
     </section>
