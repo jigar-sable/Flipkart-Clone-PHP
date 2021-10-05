32594355d9b1f99780d1d6ba349458f2c96818a3
Jigar Sable <jigarsable21@gmail.com>
2021-10-05 15:17:40 +0530

    minify css & js

--- header.php
+++ header.php
@@ -28,7 +28,7 @@ require('functions.php');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- custom build css -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
 
 
 </head>
