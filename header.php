2529f903a1ed116e7a535e532bcfc7f80923e23d
Jigar Sable <jigarsable21@gmail.com>
2021-10-06 10:15:58 +0530

    btn ui fix

--- header.php
+++ header.php
@@ -34,7 +34,7 @@ require('functions.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
     <!-- custom build css -->
-    <link rel="stylesheet" href="assets/css/style.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     
     <!-- jquery cdn -->
     <script src="assets/js/jquery-3.6.0.min.js"></script>
@@ -51,7 +51,7 @@ require('functions.php');
 
         <!-- logo & search container -->
         <div class="flex items-center flex-1">
-        <a class="h-7 mr-4" href="/Flipkart-Clone-PHP/index.php">
+        <a class="h-7 mr-4" href="<?php echo $URL; ?>/index.php">
             <img draggable="false" loading="lazy" class="h-full w-full object-contain" src="assets/images/logo.webp" alt="Logo">
         </a>
 
