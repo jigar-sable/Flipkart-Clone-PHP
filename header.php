7654a598055a1cf6b73edfb2851eb874c38cfd78
Jigar Sable <jigarsable21@gmail.com>
2021-10-07 10:55:38 +0530

    minified

--- header.php
+++ header.php
@@ -34,7 +34,7 @@ require('functions.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
     <!-- custom build css -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
     
     <!-- jquery cdn -->
     <script src="assets/js/jquery-3.6.0.min.js"></script>
